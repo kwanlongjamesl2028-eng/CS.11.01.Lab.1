9374c428dfa317d62b860af4e27ba377241b2724
kwanlongjamesl2028-eng <kwanlongjamesl2028@student.cis.edu.hk>
2025-09-15 12:03:29 +0800

    Finished task

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the age: ");
+        int age = scanner.nextInt();
+        int thing = age/2 +7;
+        System.out.println(age+"-year olds should date somebody who is at least " + thing +" years old");
+        System.out.println();
+
         
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        int thing = 100 - age;
+        System.out.println(age+ "-year olds should read at least "+ thing+" pages before giving up on a book");
     }
 }
+
+
+
+
+
+
+
+
+
+
